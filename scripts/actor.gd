e15a9f45025ed2799c4d95a9eaaa5523ebb77dcf
magelon <alfardproject@gmail.com>
2020-04-26 17:19:07 -0700

    c

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 class_name actor
 
+#limite speed so character will not out of control
 export var speed:= Vector2(300.0,1000.0)
 export var gravity:= 3000.0
 var velocity: = Vector2.ZERO
@@ -8,4 +9,6 @@ var velocity: = Vector2.ZERO
 					#delta smooth per frame
 func _physics_process(delta: float) -> void:
 	velocity.y+=gravity*delta
+	
+	#move and slide build in function works to smooth frame rate
 	velocity=move_and_slide(velocity)
