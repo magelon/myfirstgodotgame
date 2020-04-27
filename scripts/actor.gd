30222299baebb000ab2f896cde7b1d23bf7fb4a1
magelon <alfardproject@gmail.com>
2020-04-26 18:38:22 -0700

    c

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -5,10 +5,6 @@ class_name actor
 export var speed:= Vector2(300.0,1000.0)
 export var gravity:= 3000.0
 var velocity: = Vector2.ZERO
-
-					#delta smooth per frame
-func _physics_process(delta: float) -> void:
-	velocity.y+=gravity*delta
+#move and slide
+const FLOOR_NORMAL: =Vector2.UP	
 	
-	#move and slide build in function works to smooth frame rate
-	velocity=move_and_slide(velocity)
