cde868e17710f1f4cea11eaeb22a77137b7adb1f
magelon <alfardproject@gmail.com>
2020-05-18 12:09:36 -0700

    c

---
+++ scripts/Editor_Object.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var can_place=true
+onready var level=get_node("/root/main/level")
+
+var current_item
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	global_position=get_global_mouse_position()
+	if(current_item!=null and can_place and Input.is_action_just_pressed("mb_left")):
+		var new_item=current_item.instance()
+		level.add_child(new_item)
+		new_item.global_position=get_global_mouse_position()
+		pass
