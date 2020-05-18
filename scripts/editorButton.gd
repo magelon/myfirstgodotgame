e314d88a2ca5943519baa72fdb88129dcd8abc74
magelon <alfardproject@gmail.com>
2020-05-18 13:10:28 -0700

    c

--- scripts/editorButton.gd
+++ scripts/editorButton.gd
@@ -4,4 +4,4 @@ extends Button
 
 func _on_button_up() -> void:
 	#change theme
-	get_tree().change_scene("res://scens/levels/level1.tscn")
+	get_tree().change_scene("res://scens/editor_Folder/level_editor.tscn")
