9d09c516f0b02c1e785045e7822c57d53f141196
magelon <alfardproject@gmail.com>
2020-05-19 13:57:17 -0700

    c

--- scripts/level_Editor/TabContainer.gd
+++ scripts/level_Editor/TabContainer.gd
@@ -5,6 +5,12 @@ onready var object_cursor=get_node("/root/main/Editor_Object")
 func _ready() -> void:
 	pass
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_editor"):
+		Global.playing=!Global.playing
+		visible=!Global.playing
+	pass	
+
 func _on_TabContainer_mouse_entered():
 	object_cursor.can_place=false
 	object_cursor.hide()
